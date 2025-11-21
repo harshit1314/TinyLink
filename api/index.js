43ba0e924074805b44c873585063bd30e70576b8
Harshit Agarwal <harshitagarwal038@gmail.com>
2025-11-21 23:30:24 +0530

    Improve serverless handler exports; make client fetch resilient to non-JSON responses

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 const serverless = require('serverless-http');
 const app = require('../server');
 
-module.exports = serverless(app);
+// Export multiple handler forms to improve compatibility with Vercel and other hosts.
+const handler = serverless(app);
+module.exports = handler;
+module.exports.handler = handler;
+module.exports.default = handler;
+
+// Also export a plain Express-style handler (some platforms call the file directly)
+module.exports.raw = (req, res) => app(req, res);
